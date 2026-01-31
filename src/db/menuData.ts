8990716b6d814a3306701e2d8f81da7d305afdf9
AhmedHassan-Ahmed <ahmedhassan8754321@gmail.com>
2026-01-31 03:55:06 +0200

    Build ready

--- src/db/menuData.ts
+++ src/db/menuData.ts
@@ -52,21 +52,9 @@ export const menuData: MenuItemDataType[] = [
       {
         title: 'Projects',
         link: '/project',
-        submenu: [
-          { title: 'Project', link: '/project' },
-          { title: 'Project Carousel', link: '/project-carousel' },
-          { title: 'Project Details', link: '/project-details' },
-        ],
+        
       },
-      {
-        title: 'Team',
-        link: '/team',
-        submenu: [
-          { title: 'Our Team', link: '/team' },
-          { title: 'Team Carousel', link: '/team-carousel' },
-          { title: 'Team Details', link: '/team-details' },
-        ],
-      },
+      
       { title: "Pricing", link: '/pricing' },
       { title: "Faq's", link: '/faq' },
       { title: '404 Page', link: '/404' },
@@ -75,11 +63,11 @@ export const menuData: MenuItemDataType[] = [
   {
     title: 'Lastest News',
     link: '/news',
-    submenu: [
-      { title: 'Blog Grid', link: '/news' },
-      { title: 'Blog Standard', link: '/news-standard' },
-      { title: 'Blog Details', link: '/news-details' },
-    ],
+   
+  },
+  {
+    title: 'careers',
+    link: '/contact',
   },
   {
     title: 'Contact',
